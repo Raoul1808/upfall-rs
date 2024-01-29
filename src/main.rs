7b3ad269edd1c9d79e32de739c3290ea672a1396
Raoul1808 <raoulthegeek@gmail.com>
2024-01-29 07:58:35 +0100

    Create tetra app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use tetra::{graphics::{self, Color}, ContextBuilder, State};
+
+struct GameState;
+
+impl GameState {
+    fn new(_ctx: &mut tetra::Context) -> tetra::Result<GameState> {
+        Ok(GameState)
+    }
+}
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut tetra::Context) -> tetra::Result {
+        graphics::clear(ctx, Color::rgb8(100, 149, 237));
+        Ok(())
+    }
+}
+
+fn main() -> tetra::Result {
+    ContextBuilder::new("Upfall", 1280, 720)
+        .show_mouse(true)
+        .build()?
+        .run(GameState::new)
 }

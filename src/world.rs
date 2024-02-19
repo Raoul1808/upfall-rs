29a75c2dbe2906d191bfadd21527f779673228e2
Raoul1808 <raoulthegeek@gmail.com>
2024-02-19 10:54:44 +0100

    Add otherside animation

--- src/world.rs
+++ src/world.rs
@@ -1,5 +1,6 @@
 use core::fmt;
 
+use keyframe::{ease, functions::EaseOutQuart, num_traits::clamp};
 use tetra::{
     graphics::{self, BlendState, Color, DrawParams, Rectangle},
     input::{self, Key},
@@ -41,6 +42,63 @@ impl WorldMode {
     }
 }
 
+struct OthersideAnimator {
+    size_anim_progress: f32,
+    full_circle_size: f32,
+    circle_size: f32,
+    mode: WorldMode,
+    size_passive_accumulator: f32,
+}
+
+impl OthersideAnimator {
+    const SIZE_PASSIVE_SIN_MOD: f32 = 5.;
+    const CIRCLE_GROW_TIME: f32 = 0.25;
+    const CIRCLE_SHRINK_TIME: f32 = 0.125;
+    const CIRCLE_SIZE: f32 = 75.;
+
+    pub fn new() -> Self {
+        Self {
+            size_anim_progress: 0.,
+            full_circle_size: 0.,
+            circle_size: 0.,
+            mode: WorldMode::Dark,
+            size_passive_accumulator: 0.,
+        }
+    }
+
+    pub fn world_change(&mut self, world: WorldMode) {
+        self.mode = world;
+    }
+
+    pub fn update(&mut self, dt: f32) {
+        let cond = match self.mode {
+            WorldMode::Dark => self.size_anim_progress > 0.,
+            WorldMode::Light => self.size_anim_progress < 1.,
+        };
+        if cond {
+            match self.mode {
+                WorldMode::Dark => {
+                    self.size_anim_progress -= dt / Self::CIRCLE_SHRINK_TIME;
+                }
+                WorldMode::Light => {
+                    self.size_anim_progress += dt / Self::CIRCLE_GROW_TIME;
+                }
+            }
+            let t = clamp(self.size_anim_progress, 0., 1.);
+            self.circle_size = ease(EaseOutQuart, 0., self.full_circle_size, t);
+        } else if self.mode == WorldMode::Light {
+            self.circle_size = self.full_circle_size;
+        }
+        self.size_passive_accumulator += dt;
+        self.full_circle_size =
+            Self::CIRCLE_SIZE + self.size_passive_accumulator.sin() * Self::SIZE_PASSIVE_SIN_MOD;
+    }
+
+    pub fn get_circle_size(&self) -> f32 {
+        self.circle_size
+    }
+}
+
 pub struct World {
     player: Player,
     dark_tilemap: Tilemap,
@@ -53,6 +111,7 @@ pub struct World {
     keys_amount: usize,
     got_keys: usize,
     win: bool,
+    otherside: OthersideAnimator,
 }
 
 impl World {
@@ -78,6 +137,7 @@ impl World {
             light_keys: Vec::new(),
             keys_amount,
             got_keys: 0,
+            otherside: OthersideAnimator::new(),
         }
     }
 
@@ -85,6 +145,7 @@ impl World {
         self.win = false;
         self.player = Player::new(self.spawn_pos);
         self.mode = WorldMode::Dark;
+        self.otherside = OthersideAnimator::new();
         for pos in &self.dark_keys {
             self.dark_tilemap.set_tile_usize(*pos, Tile::Key);
         }
@@ -103,6 +164,7 @@ impl World {
     }
 
     pub fn update(&mut self, ctx: &mut tetra::Context) {
+        let dt = tetra::time::get_delta_time(ctx).as_secs_f32();
         if self.win {
             return;
         }
@@ -130,6 +192,7 @@ impl World {
                     if self.player.can_traverse_portal(rect, *axis) {
                         self.mode.switch();
                         self.player.on_world_change(self.mode);
+                        self.otherside.world_change(self.mode);
                     }
                 }
                 Tile::Key => {
@@ -139,6 +202,7 @@ impl World {
         }
 
         self.player.post_update();
+        self.otherside.update(dt);
 
         let player_rect = self.player.get_hbox();
         if self.got_keys == self.keys_amount && player_rect.intersects(&self.end_rect) {
@@ -167,15 +231,9 @@ impl World {
 
     pub fn draw(&self, ctx: &mut tetra::Context, assets: &Assets) {
         graphics::set_blend_state(ctx, BlendState::add(false));
-        assets.shader.set_uniform(
-            ctx,
-            "u_circle_radius",
-            if self.mode == WorldMode::Light {
-                50.0
-            } else {
-                0.0
-            },
-        );
+        assets
+            .shader
+            .set_uniform(ctx, "u_circle_radius", self.otherside.get_circle_size());
         let player_hbox = self.player.get_hbox();
         assets
             .shader
